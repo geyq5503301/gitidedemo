1447f61ec55f4e3005e77400af1982a85376f91e
Fir <570002759@qq.com>
2021-04-18 18:47:27 +0800

    firstgit

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -2,5 +2,6 @@ public class GitDemo {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("123456");
+        System.out.println("99999");
     }
 }
